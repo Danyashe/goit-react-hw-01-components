eb420e746d50877aae4866578bd77e0db83358f0
Danyashe <27shdaniil@gmail.com>
2023-05-07 13:08:06 +0300

    add friends styled

--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -1,11 +1,13 @@
 import { ItemFriend, Status, Avatar, NameFriend } from './friendList.styled';
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
-  <ItemFriend>
-    <Status>{isOnline}</Status>
-    <Avatar src={avatar} alt="User avatar" width="48" />
-    <NameFriend>{name}</NameFriend>
-  </ItemFriend>;
+  return (
+    <ItemFriend>
+      <Status>{isOnline}</Status>
+      <Avatar src={avatar} alt="User avatar" width="48" />
+      <NameFriend>{name}</NameFriend>
+    </ItemFriend>
+  );
 };
 
 export default FriendListItem;
--- src/components/FriendList/friendList.styled.jsx
+++ src/components/FriendList/friendList.styled.jsx
@@ -1,7 +1,30 @@
 import styled from '@emotion/styled';
 
-export const ItemFriend = styled.li``;
-export const Status = styled.span``;
+export const ItemFriend = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 9px;
+  padding: 8px;
+  width: 250px;
+  border: 2px solid black;
+  border-radius: 5px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const Status = styled.span`
+  display: block;
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  background-color: ${props => (props.isOnline ? 'green' : 'red')};
+`;
+
 export const Avatar = styled.img``;
 export const NameFriend = styled.p``;
-export const ListFriend = styled.ul``;
+export const ListFriend = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
